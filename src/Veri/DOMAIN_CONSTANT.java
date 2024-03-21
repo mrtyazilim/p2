ee5d6b78a3d8ffa6e649b57d79b95fca0907c669
Murat.bt <bt.mrt@yandex.com>
2024-03-21 13:12:50 +0300

    Update DOMAIN_CONSTANT.java
    github tarayıcıda userdatapath i değiştim.

--- src/Veri/DOMAIN_CONSTANT.java
+++ src/Veri/DOMAIN_CONSTANT.java
@@ -2,7 +2,7 @@ package Veri;
 
 public class DOMAIN_CONSTANT {
    // public static final String OBJECTDATAPATH="Veri/Nesne/denememetin.txt";
-    public static final String USERDATAPATH="src/Veri/Nesne/denememetin";
+    public static final String USERDATAPATH="src/Veri/Nesne/User.txt";
            //"Veri/Nesne/denememetin.txt";
                         //M1K1Proje1/untitled/src/Veri/Nesne
     public final static String TRANSACTION_DATAPATH="src/Veri/Olaylar/";
